97f27b5408218967dc37cf6a31a17998dd42c236
John-Mukhwana <johnbradill67@gmail.com>
2025-09-03 10:25:50 +0300

    Enhance comments for contract layout and structure in VoteStorage

--- src/VoteStorage.sol
+++ src/VoteStorage.sol
@@ -1,6 +1,31 @@
 
 //SPDX-License-Identifier: MIT
 
+
+// Layout of the contract file:
+// version
+// imports
+// errors
+// interfaces, libraries, contract
+
+// Inside Contract:
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+
 contract VoteStorage {
     // Vote storage logic
 }
