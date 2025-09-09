1be340c4f3a8e8d0f85b54508023ee8c4546f9e3
John-Mukhwana <johnbradill67@gmail.com>
2025-09-09 21:16:41 +0300

    Add OwnableUpgradeable import to VoteStorage contract for enhanced access control

--- src/VoteStorage.sol
+++ src/VoteStorage.sol
@@ -28,6 +28,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 contract VoteStorage {
     // Vote storage logic
 }
