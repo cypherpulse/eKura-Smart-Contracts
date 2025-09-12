03bed6521bf631a22e7c6a2c0b7a5a77e0a02524
John-Mukhwana <johnbradill67@gmail.com>
2025-09-12 21:09:01 +0300

    Add contract definition for HelperConfig with initial structure

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,9 +3,14 @@
 
 pragma solidity ^0.8.20;
 
+import {Script} from "forge-std/Script.sol";
 /***
  * @title HelperConfig
  * @author cypherpulse.base.eth
  * @notice This contract helps manage configurations for different networks
  * @dev Handles deployment parameters for local, testnet, and mainnet environments
  */
+
+contract HelperConfig{
+
+}
