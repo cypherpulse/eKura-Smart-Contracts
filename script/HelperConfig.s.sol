7c956420c57c80faf45f0aab7fbfe926bceb63ee
John-Mukhwana <johnbradill67@gmail.com>
2025-09-12 21:14:37 +0300

    Add type declarations section in HelperConfig contract

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -14,4 +14,9 @@ import {Script} from "forge-std/Script.sol";
 contract HelperConfig is Script{
     //Errors//
     error HelperConfig__InvalidChainId();
+
+    //Type Declarations
+    struct NetworkConfig{
+        
+    }
 }
