b674ced390044d8f115481e547cea47d11d4f131
John-Mukhwana <johnbradill67@gmail.com>
2025-09-12 21:07:35 +0300

    Update documentation in HelperConfig contract for clarity

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -6,5 +6,6 @@ pragma solidity ^0.8.20;
 /***
  * @title HelperConfig
  * @author cypherpulse.base.eth
- * @notice This contract 
+ * @notice This contract helps manage configurations for different networks
+ * @dev Handles deployment parameters for local, testnet, and mainnet environments
  */
