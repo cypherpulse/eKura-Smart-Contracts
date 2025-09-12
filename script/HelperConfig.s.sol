7de43954d57a193b5a064fefbd00ff4fc674b544
John-Mukhwana <johnbradill67@gmail.com>
2025-09-12 21:31:09 +0300

    Fix formatting issue by adding missing state variable declaration in HelperConfig contract

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -31,4 +31,5 @@ contract HelperConfig is Script{
     uint256 public constant ETH_SEPOLA_CHAIN_ID =11155111;
     uint256 public constant BASE_MAINNET_CHAIN_ID =8453;
     uint256 public constant BASE_SEPOLIA_CHAIN_ID =84531;
- 
+    uint256 public constant LOCALHOST_CHAIN_ID =31337;
+}
