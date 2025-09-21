2f372cc5f6e15403aa36a097393e20d94a97dc34
John-Mukhwana <johnbradill67@gmail.com>
2025-09-21 20:15:22 +0300

    Fix import statement in DeployElectionFactory.s.sol to ensure proper functionality

--- script/DeployElectionFactory.s.sol
+++ script/DeployElectionFactory.s.sol
@@ -4,4 +4,5 @@
 pragma solidity ^0.8.20;
 
 import {Script,console} from "forge-std/Script.sol";
-import {}
+import {ElectionFactory} from "../src/ElectionFactory.sol";
+import {HelperConfig} from "./HelperConfig.s.sol"
