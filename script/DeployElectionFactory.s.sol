9723e348d97c952fe018b081473b0e9aa59e4e6b
John-Mukhwana <johnbradill67@gmail.com>
2025-09-21 20:25:36 +0300

    Fix formatting in DeployElectionFactory.s.sol by adding missing newline and updating comments

--- script/DeployElectionFactory.s.sol
+++ script/DeployElectionFactory.s.sol
@@ -6,3 +6,8 @@ pragma solidity ^0.8.20;
 import {Script,console} from "forge-std/Script.sol";
 import {ElectionFactory} from "../src/ElectionFactory.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+
+/**
+ * @title DeployElectionFactory
+ * @author cypherpulse.base.eth
+ */
