527bde09af8d0062746fd41f09046d57a3cc01a9
John-Mukhwana <johnbradill67@gmail.com>
2025-09-21 20:21:51 +0300

    Fix import statement in DeployElectionFactory.s.sol by adding missing semicolon

--- script/DeployElectionFactory.s.sol
+++ script/DeployElectionFactory.s.sol
@@ -5,4 +5,4 @@ pragma solidity ^0.8.20;
 
 import {Script,console} from "forge-std/Script.sol";
 import {ElectionFactory} from "../src/ElectionFactory.sol";
-import {HelperConfig} from "./HelperConfig.s.sol"
+import {HelperConfig} from "./HelperConfig.s.sol";
