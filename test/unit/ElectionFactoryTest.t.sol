c831754c19a482a78a58b0442a4a14e6b52ae4a2
John-Mukhwana <johnbradill67@gmail.com>
2025-09-29 00:09:48 +0300

    Add missing import statement for DeployElectionFactory in ElectionFactoryTest

--- test/unit/ElectionFactoryTest.t.sol
+++ test/unit/ElectionFactoryTest.t.sol
@@ -4,3 +4,10 @@ pragma solidity ^0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {ElectionFactory} from "../../src/ElectionFactory.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DeployElectionFactory} from "../../script/DeployElectionFactory.s.sol";
+
+contract ElectionFactoryTest is Test{
+    //EVENTS//
+
+    
+}
