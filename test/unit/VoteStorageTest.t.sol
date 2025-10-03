9027f96138b27eb6981f58888c79375dc5ac2959
John-Mukhwana <johnbradill67@gmail.com>
2025-10-03 16:07:11 +0300

    Fix import statement syntax in VoteStorageTest.t.sol by adding missing newline and including DeployElectionFactory import

--- test/unit/VoteStorageTest.t.sol
+++ test/unit/VoteStorageTest.t.sol
@@ -4,3 +4,4 @@ pragma solidity ^0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {VoteStorage} from "../../src/VoteStorage.sol";
 import {ElectionFactory} from "../../src/ElectionFactory.sol";
+import {DeployElectionFactory} from "../../script/DeployElectionFactory.s.sol";
