0982f90ca01adb898c9a03c162f7ad91f0021168
John-Mukhwana <johnbradill67@gmail.com>
2025-10-03 16:12:12 +0300

    Fix import statement syntax in VoteStorageTest.t.sol by adding missing newline and including ERC1967Proxy import

--- test/unit/VoteStorageTest.t.sol
+++ test/unit/VoteStorageTest.t.sol
@@ -6,3 +6,4 @@ import {VoteStorage} from "../../src/VoteStorage.sol";
 import {ElectionFactory} from "../../src/ElectionFactory.sol";
 import {DeployElectionFactory} from "../../script/DeployElectionFactory.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
