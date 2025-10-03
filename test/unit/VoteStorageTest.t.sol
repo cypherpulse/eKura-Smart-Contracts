2f1d1235e0becd9c6bc9a8e86087e92681f34050
John-Mukhwana <johnbradill67@gmail.com>
2025-10-03 18:32:49 +0300

    mock voter trying to vote

--- test/unit/VoteStorageTest.t.sol
+++ test/unit/VoteStorageTest.t.sol
@@ -206,7 +206,9 @@ contract VoteStorageTest is Test {
 
     function test_Vote_RevertWhen_Paused() public {
         // Pause Contract
-        vm.prank(platformAdmin)
+        vm.prank(platformAdmin);
         voteStorage.pause();
+
+        vm.prank(voter1);
     } 
 }
