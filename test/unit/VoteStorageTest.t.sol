841cd7582974553b5e2041dcfa15f78d3c97ce0c
John-Mukhwana <johnbradill67@gmail.com>
2025-10-03 16:21:12 +0300

    Add VoteStorageTest contract and event declaration in VoteStorageTest.t.sol

--- test/unit/VoteStorageTest.t.sol
+++ test/unit/VoteStorageTest.t.sol
@@ -14,3 +14,11 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
  * @notice Unit tests for the VoteStorage contract
  * @dev Testing upgradeable contract with proxy pattern
  */
+
+contract VoteStorageTest is Test {
+    // EVENTS //
+
+    event VoteCast(
+        
+    )
+}
