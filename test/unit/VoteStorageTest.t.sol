cde55852590fa0c64ea9c899191b90117b0bbc3d
John-Mukhwana <johnbradill67@gmail.com>
2025-10-04 20:14:29 +0300

    Added voter initialization in test_VoteWithSignature_RevertWhen_ExpiredSignature

--- test/unit/VoteStorageTest.t.sol
+++ test/unit/VoteStorageTest.t.sol
@@ -253,7 +253,7 @@ contract VoteStorageTest is Test {
         uint256 deadline = block.timestamp - 1; // Expired
 
         VoteStorage.VoteData memory voteData = VoteStorage.VoteData({
-            
+            voter: voter1;
         })
     }
 }
